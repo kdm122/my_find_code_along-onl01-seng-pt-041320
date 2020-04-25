ba81cf898a8192456bdcd06f7c0996cab6dc4f66
Kyle McLean <kyledmclean@gmail.com>
2020-04-25 17:53:14 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,12 +6,12 @@ def my_find(collection)
   i = 0 
   while i < collection.length
 
-  puts collection[i] if yield(collection[i])
+  return collection[i] if yield(collection[i])
    i = i + 1 
  end
 
 end
 
-collection = (1..100).to_a
-my_find(collection) {|i| i % 4 == 0 and i % 6 == 0}
+#collection = (1..100).to_a
+#my_find(collection) {|i| i % 4 == 0 and i % 6 == 0}
 
